614483e4278d0181ae56d3d37c0eca6965b76d12
Dielson Sales <dielson@ilhasoft.com.br>
2019-09-10 17:54:12 -0300

    Change main.py to allow testing multiple phrases

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from rasa_nlu.model import Interpreter
 import json
 interpreter = Interpreter.load("./models/current/nlu")
-message = "qro enviar elogio"
-result = interpreter.parse(message)
-print(json.dumps(result, indent=2))
+
+while True:
+    print("Type a phrase:")
+    message = input()
+    result = interpreter.parse(message)
+    print(json.dumps(result, indent=2))
